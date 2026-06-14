1c9231367f60b97ee1baaa6d4c8cc5ad6ea0d6ea
Chirag-Tulani <tulanichirag7@gmail.com>
2026-06-14 12:42:46 +0530

    My first program -> Hello World

---
+++ youtube/Main.java
@@ -0,0 +1,20 @@
+package youtube;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // print -> prints the value on same line.
+        /*  println -> prints the value in different lines.
+         \n -> moove the test to another line. */
+
+        // -> single line comment.
+        // /* */ -> multi-line comment.
+        // sout -> for writing System.out.print line fast.
+
+        System.out.print("Hello, World !!");
+        System.out.print("Hello \n");
+        System.out.println("It is my first program.");
+        System.out.print("WOW");
+        
+    }
+}

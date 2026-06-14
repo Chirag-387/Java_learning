8b245462664eed3bc104e447ba4b86ff92170504
Chirag-Tulani <tulanichirag7@gmail.com>
2026-06-14 15:18:06 +0530

    Add variable file which contains variable decleration and datatypes in JAVA.

---
+++ youtube/variables.java
@@ -0,0 +1,54 @@
+package youtube;
+
+public class variables {
+    public static void main(String[] args) {
+
+    /*
+    Steps for creating a variable are ;
+    1. Declaration
+    2. Assignment
+    */
+
+
+    // 1. Primtive Datatypes
+
+    // A. Integer datatype
+    int year = 2026;
+    System.out.println(year);
+    System.out.println("The year is " + 2026);
+
+    // B. Double datatype
+    double price = 24.99;
+    double temp = -12.5;
+    System.out.println("The price of the bag is $" + price);
+    System.out.println("The temperature is " + temp);
+
+    // C. Character datatype
+    char grade = 'A';
+    char symbol = '$';
+    System.out.println("My grade for this term exam is " + grade);
+    System.out.println("USA currency symbol is " + symbol);
+
+    // D. Boolean datatype
+    boolean isStudent = true;
+    boolean isWorking = false;
+    System.out.println(isStudent);
+    System.out.println(isWorking);
+
+    // 2. Reference Dataypes
+
+    // A. String datatype
+    String name = "Chirag";
+    String color = "Deep Blue";
+    String email = "fake123@gmail.com";
+    System.out.println("My name is " + name);
+    System.out.println("My facourite color is " + color + " .");
+    System.out.println("My FAKE email address is " + email + " .");
+
+    // B. Array datatype
+    // C. Object datatype
+
+    // Both array and object will be cover in future.
+
+    }
+}

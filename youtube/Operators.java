fae072736e2143b3c67653c137ed00755e03246e
Chirag-Tulani <tulanichirag7@gmail.com>
2026-06-16 12:30:04 +0530

    Added Arithmetic operators file.

---
+++ youtube/Operators.java
@@ -0,0 +1,52 @@
+package youtube;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        // ARITHMETIC OPERATORS
+
+        // int x = 3;
+        // int y = 7;
+        // int z;
+
+        // z = x + y; Addition
+        // z = x - y; Subtraction
+        // z = x * y; Multiplication
+        // z = x / y Division
+        // z = x % y; Modulus(Gives the remainder of the division)
+
+        // System.out.println(z);
+
+        // AUGMENTED ASSISGNMENT OPERATORS
+
+        // int x = 10;
+        // int y = 3;
+
+        // x = x + y;
+        //OR
+        // x += y;
+
+        // Same is for Subtraction, Multiplication, Division and Modulus.
+
+        // System.out.println(x);
+
+        // INCREMENT AND DECREMENT OPERATORS
+
+        // int x = 1;
+
+        // x++; -> Increases by one
+        // x-- -> Decreases by one
+
+        // System.out.println(x);
+
+        // ORDER OF OPERATIONS [P-E-M-D-A-S]
+
+        // P -> Brackets
+        // E -> Exponents
+        // M -> Multiplication
+        // D -> Division
+        // A -> Addition
+        // S -> Subtraction
+
+    }
+}

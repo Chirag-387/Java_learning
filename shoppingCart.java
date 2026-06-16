066edc8f02585aaae8f7d4f3b9d5e975f6f3e631
Chirag-Tulani <tulanichirag7@gmail.com>
2026-06-16 12:55:15 +0530

    Shopping cart mini project

---
+++ shoppingCart.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class shoppingCart {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        System.out.print("What do you want to buy: ");
+        item = input.nextLine();
+
+        System.out.print("what's the price : ");
+        price = input.nextDouble();
+        input.nextLine();
+
+        System.out.print("How many : ");
+        quantity = input.nextInt();
+        input.nextLine();
+
+        total = quantity * price;
+
+        System.out.println("You buy " + quantity + " " + item + " so total is " + total + currency + ".");
+
+        input.close();
+    }
+}

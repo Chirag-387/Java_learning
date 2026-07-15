ee4b44f2319683504e222314e2c49f1bc1a1eb58
Chirag-387 <chrgtln@gmail.com>
2026-07-15 10:04:47 +0530

    Added new practice file of strings in java.

---
+++ practice/other/strings2.java
@@ -0,0 +1,131 @@
+package practice.other;
+import java.util.Scanner;
+
+public class strings2 {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        // 1. Check if two strings are equal.
+
+
+        // // take user input for string 1.
+        // System.out.print("Enter first string: ");
+        // String str1 = scanner.nextLine();
+
+        // System.out.println();
+
+        // // take user input for string 2.
+        // System.out.print("Enter second string: ");
+        // String str2 = scanner.nextLine();
+
+        // System.out.println();
+
+        // // string 1.
+        // System.out.printf("The first string is %s.\n", str1);
+
+        // // string 2.
+        // System.out.printf("the second string is %s.\n", str2);
+
+        // System.out.println();
+
+        // if(str1.equals(str2)) {
+        //     System.out.printf("The %s and %s are equal.\n", str1, str2);
+        // }
+        // else {
+        //     System.out.printf("The %s and %s are not equal.\n", str1, str2);
+        // }
+
+
+        // 2. Compare two strings lexicograhically.
+
+
+        // // take user input for string 1.
+        // System.out.print("Enter first string: ");
+        // String str1 = scanner.nextLine();
+
+        // System.out.println();
+
+        // // take user input for string 2.
+        // System.out.print("Enter second string: ");
+        // String str2 = scanner.nextLine();
+
+        // System.out.println();
+
+        // // string 1.
+        // System.out.printf("The first string is %s.\n", str1);
+
+        // // string 2.
+        // System.out.printf("the second string is %s.\n", str2);
+
+        // System.out.println();
+
+        // System.out.println(str1.compareTo(str2));
+
+
+        // 3. Find first occurence of a character.
+
+
+        // System.out.print("Enter a string: ");
+        // String str1 = scanner.nextLine();
+
+        // System.out.println();
+
+        // System.out.print("Enter the character to find its first occurence: ");
+        // char target = scanner.nextLine().charAt(0);
+
+        // int index = str1.indexOf(target);
+        // System.out.println(index);
+
+
+        // 4. Find the last occurence of a character.
+
+
+        // System.out.print("Enter a string: ");
+        // String str1 = scanner.nextLine();
+
+        // System.out.println();
+
+        // System.out.print("Enter a character to find its last ocuurence: ");
+        // char target = scanner.nextLine().charAt(0);
+
+        // System.out.println();
+
+        // int index = str1.lastIndexOf(target);
+        // System.out.println(index);
+
+
+        // 5. Check if a string starts with a given prefix.
+
+
+        // System.out.print("Enter a string: ");
+        // String str = scanner.nextLine();
+
+        // System.out.println();
+
+        // System.out.print("Enter a prefix to check: ");
+        // String prefix = scanner.nextLine();
+
+        // System.out.println();
+
+        // StringBuilder newStr = new StringBuilder();
+
+        // for(int i = 0; i < prefix.length(); i++) {
+        //     newStr.append(str.charAt(i));
+        // }
+
+        // System.out.println(newStr);
+
+        // System.out.println();
+
+        // if(prefix.equals(newStr)) {
+        //     System.out.println("String starts with given prefix.");
+        // }
+        // else {
+        //     System.out.println("String doesn't start with given prefix.");
+        // }
+        
+        scanner.close();
+
+    }
+}
